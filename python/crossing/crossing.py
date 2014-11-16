8bbd48e67ef0a5c8fdb628926d8d1ed445ff04c4
佐藤比香留 <hikaru@Hikaru-Mac.local>
2014-11-16 18:16:15 +0900

    create folder "python"

---
+++ python/crossing/crossing.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+f = open('file/crossing.txt')
+data = f.read()
+f.close()
+
+lines = data.split()
+array = []
+result = 0
+
+for line in lines:
+    result = result + len([x for x in array if x > line])
+    array.append(line)
+
+print str(result)
+
